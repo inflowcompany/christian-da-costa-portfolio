6a5c4eb08a5fc4d41ffd3f9b7abb41b159218665
inflowcompany <163485631+inflowcompany@users.noreply.github.com>
2026-09-06 14:58:33 -0300

    fix: link portfolio GitHub repository

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -25,7 +25,7 @@ const social: Social = [
   {
     name: "GitHub",
     icon: "github",
-    link: "https://github.com/",
+    link: "https://github.com/inflowcompany/christian-da-costa-portfolio",
     essential: true,
   },
 ];
